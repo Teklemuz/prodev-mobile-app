24eeec28f46296a259608a14dd7a817452378c6c
Teklemuz Ayenew <111185891+Teklemuz@users.noreply.github.com>
2025-03-02 21:43:36 +0300

    Update _layout.tsx

--- prodev-mobile-app-0x04/app/_layout.tsx
+++ prodev-mobile-app-0x04/app/_layout.tsx
@@ -1,10 +1,10 @@
-import { View } from "react-native";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* Add your screens here */}
+      <Stack.Screen name="join" />
+      <Stack.Screen name="signing" />
     </Stack>
   );
 }
