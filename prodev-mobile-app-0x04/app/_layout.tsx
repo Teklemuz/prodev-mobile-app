76fc3e08cddc612f8d31114c4d34184226c13b2c
Teklemuz Ayenew <111185891+Teklemuz@users.noreply.github.com>
2025-03-02 21:35:50 +0300

    Update _layout.tsx

--- prodev-mobile-app-0x04/app/_layout.tsx
+++ prodev-mobile-app-0x04/app/_layout.tsx
@@ -1,11 +1,8 @@
-"import { View } from",
-  "import { Stack } from", 
-  "export default function RootLayout()",
-    "<Stack screenOptions={{\r\n      
-  headerShown: false,\r\n    }}>", 
-    "join", "</Stack>", "signing"
-"<Stack screenOptions={{\r\n      
-  headerShown: false,\r\n    
-}
-}
->"
+import { View } from,
+  import { Stack } from, 
+  export default function RootLayout(),
+    <Stack screenOptions={{\r\n      
+  headerShown: false,\r\n  }}>, 
+    join, </Stack>, signing
+<Stack screenOptions={{\r\n      
+  headerShown: false,\r\n }}>
